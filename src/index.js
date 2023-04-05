ec3e8dd273169ae9d3bbec07b10dc4fe3beeda81
CElizOwens <owens.christina@gmail.com>
2023-04-05 09:05:49 -0700

    Update index.js to render App using via createRoot method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
