1b26e11e69179f939879e5268c050792d0867c58
CElizOwens <owens.christina@gmail.com>
2023-04-06 15:54:31 -0700

    Added a little bit more styling

--- client/components/Concert.jsx
+++ client/components/Concert.jsx
@@ -7,7 +7,7 @@ const Concert = ({name, date, pieces, id}) => {
   console.log('name, date, pieces: ', name, date, pieces);
   const piecesJSX = [];
   for (let i = 0; i < pieces.length; i++) {
-    piecesJSX.push(<p><Piece composer={pieces[i].composer} title={pieces[i].title} key={id * 1000} /></p>)
+    piecesJSX.push(<p><Piece className='piece' composer={pieces[i].composer} title={pieces[i].title} key={id * 1000} /></p>)
   }
 
   const splitDate = date.split("T")[0];
